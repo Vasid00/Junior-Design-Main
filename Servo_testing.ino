f0514b6fabdc43d675bd6fd4e3fb990b62af69ab
Vasid00 <129792411+Vasid00@users.noreply.github.com>
2023-04-04 15:10:34 -0500

    Update Servo_testing.ino
    Added template for servo testing

--- Servo_testing.ino
+++ Servo_testing.ino
@@ -1,1 +1,41 @@
+//Author: Gideon Davis, Kaleb Sarlls
 
+#include <Servo.h> //Allows easier control of servo motors
+
+//Constants
+//Change these to the actual values
+#define SERVO_OPEN   30
+#define SERVO_CLOSED 60
+
+//Global setup
+Servo gripper; //Class declaration
+
+int servoSignal = 7; //Pin connection for servo
+int servoAngle = 0; //Current angle of servo
+
+bool moveServo, servoState; //servoState true = open, false = closed
+
+void  setup(){
+  gripper.attach(servoSignal); //connect servo
+  servoState = true;
+};
+
+void loop(){
+  //Servo
+    if(moveServo)
+    {
+      //ADD CODE FOR MOVING SERVO
+      //close servo
+      if(servoState) 
+      {
+        gripper.write(SERVO_CLOSED);
+        servoState = false;
+      }
+      //open servo
+      else
+      {
+        gripper.write(SERVO_OPEN);
+        servoState = true;
+      }
+    }
+};
