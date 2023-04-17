066b6fb9c42b5f008d6c6c641de8c286ee088eee
ice-mind <129899199+ice-mind@users.noreply.github.com>
2023-04-17 13:52:31 -0500

    Update state_testing.ino
    added psuedocode for main loop variables, general code idea presented
    Gideon can add his LCD bs later

--- state_testing.ino
+++ state_testing.ino
@@ -9,9 +9,17 @@
 #define dirPinY  4
 #define stepPinY 5
 #define stopPin 50
+#define TOFY_ADDRESS 0x30
+#define TOFZ_ADDRESS 0x31
 
 int buttonState = 0;
 int clk = 0;
+//int caseHeight = ;    //case height in mm
+bool grab2Cases = false;
+bool caseGrabbed = false;
+bool evenCount = false;
+int ySpeed, zSpeed;
+int caseCount, palletCount;
 uint8_t distY; //tofZ setup code
 uint8_t distZ;
 const int STEPS_PER_MM = 27;    // constant for steps per milimeter
@@ -25,6 +33,10 @@ Adafruit_VL6180X tofZ = Adafruit_VL6180X();
 // Min y range 9-13mm (pick-up side)
 // Max y range 417-435mm (drop-off side)
 
+void setup() {
+  //FIXME: add setup from other codes
+}
+
 /* STATES TO TEST:
 y max and min
 z max and min
@@ -33,3 +45,31 @@ case dropped
 # of cases loaded
 speed of y and z and grabbing
 */
+
+//FIXME: implement peusdocode below; will be what main loop consists of
+
+void loop(){
+  /* 
+  if (!grab2Cases){
+    for {
+      ensure y min is reached
+      lower z min
+      ensure z max is reached
+      grab bool
+      rise to z min
+      go to y max
+      if (evenCount){
+        lower to zMax                  //caseCount is even (0,2,4,6)
+      if (!evenCount){
+        lower to zMin - caseHeight      //caseCount is odd (1,3,5,7)
+      drop case
+      grabbed !bool
+      increase case count for current pallet
+      go to zMin
+    }
+    when caseCount == 8
+      delay (lets other team do thing)
+      reset caseCount
+      palletCount++
+      start main loop again 
+/*
