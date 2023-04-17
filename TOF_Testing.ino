085223616129cf3df6de9d50c516490152d10619
ice-mind <129899199+ice-mind@users.noreply.github.com>
2023-04-16 23:44:31 -0500

    Update TOF_Testing.ino
    added I2C addressing

--- TOF_Testing.ino
+++ TOF_Testing.ino
@@ -4,6 +4,9 @@
 #include <Adafruit_VL6180X.h>      // Z sensor library
 #include <Wire.h>
 
+#define TOFY_ADDRESS 0x30
+#define TOFZ_ADDRESS 0x31
+
 Adafruit_VL53L0X tofY = Adafruit_VL53L0X();
 Adafruit_VL6180X tofZ = Adafruit_VL6180X();
 
@@ -18,12 +21,12 @@ void setup() {
   }
   
   Serial.println("TOF Sensors testing.");   // test if sensors are connected
-  if (!tofY.begin())                        // kills code if both are not detected
+  if (!tofY.begin(TOFY_ADDRESS))            // kills code if both are not detected
   {
     Serial.println(F("Failed to find VL53L0X (YMV)"));
     while(1);
   }
-  if (!tofZ.begin())
+  if (!tofZ.begin(TOFZ_ADDRESS))
   {
     Serial.println(F("Failed to find VL6180X (ZMV)"));
     while(1);
@@ -31,6 +34,8 @@ void setup() {
   
   Serial.println(F("TOF Testing Start:\n\n"));  // testing start
   tofY.startRangeContinuous();                  // start continuous ranging for YMV
+  uint8_t rangeZ;
+  uint8_t statusZ;
 }
 
 
@@ -40,14 +45,11 @@ void loop() {
     Serial.println(tofY.readRange());
   }
   
-  float lux = tofZ.readLux(VL6180X_ALS_GAIN_5);   // Z setup code
-  Serial.print("Lux: ");                          // FIXME/TESTME: need to see difference in readlux and readRange functions
-  Serial.println(lux);
-  uint8_t range = tofZ.readRange();
-  uint8_t status = tofZ.readRangeStatus();
-
+  rangeZ = tofZ.readRange();
+  statusZ = tofZ.readRangeStatus();
   if (status == VL6180X_ERROR_NONE) {          // detect and give Z distance
     Serial.print(""Distance in Z: ");
     Serial.println(range);
   }
-}
+  delay(1000);
+ }
